15487e6a82954d617f9a31ed33de1f1b653c37a7
Construct <62724481+ConstructStudios@users.noreply.github.com>
2020-03-29 16:00:44 -0400

    yikes
    ...

--- src/ConstructStudios/Report/Report.php
+++ src/ConstructStudios/Report/Report.php
@@ -2,7 +2,7 @@
 
 namespace ConstructStudios\Report;
 
-use ConstructStudios\ReportToDiscord\Report;
+use ConstructStudios\ReportToDiscord\PGTD;
 use ConstructStudios\Report\commands\AdminCommand;
 use ConstructStudios\Report\commands\ReportCommand;
 use ConstructStudios\Report\commands\ReportListCommand;
